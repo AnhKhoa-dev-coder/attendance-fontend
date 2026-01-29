6465ca68064ce484629ac229d0c88f54d2a0800c
Cyber ​​Security <xaanhkhoa1805@gmail.com>
2026-01-29 20:06:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const video = document.getElementById("video");
 const statusBox = document.getElementById("status");
 
 const API_BASE = " https://michal-challenging-misti.ngrok-free.dev";
+// IP máy tính
 
 async function startCamera() {
   try {
@@ -46,14 +47,71 @@ function start() {
     .then((data) => {
       statusBox.innerText =
         data.status === "success"
-          ? "✅ Điểm danh thành công - Cảm On Bạn Rất Nhiều"
+          ? "✅ Điểm danh thành công"
           : "❌ " + data.message;
     })
     .catch((err) => {
       console.error(err);
-      statusBox.innerText = "❌ Không kết nối được server - Bộ phận kĩ thuật chúng tôi hiện tại đã tắt server vui lòng liên hệ kĩ thuât để được giải quyết";
+      statusBox.innerText =
+        "❌ Không kết nối được server - Bộ phận kĩ thuật chúng tôi hiện tại đã tắt server liên hệ kĩ thuât --> ( Anh Khoa ) để được giải quyết";
     });
 }
+function checkAttendance() {
+  const code = document.getElementById("searchCode").value.trim();
+  const dateInput = document.getElementById("searchDate").value.trim();
+  const resultBox = document.getElementById("searchResult");
 
+  if (!code || !dateInput) {
+    resultBox.innerText = "❗ Vui lòng nhập đủ mã và ngày";
+    return;
+  }
 
+  const date = normalizeDate(dateInput);
 
+  if (!date) {
+    resultBox.innerText = "❌ Ngày không hợp lệ (vd: 1/1/2025)";
+    return;
+  }
+
+  const API_BASE = "http://192.168.1.116:5000";
+
+  fetch(`${API_BASE}/check?code=${code}&date=${date}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const img = document.getElementById("resultImage");
+
+      if (data.status === "found") {
+        resultBox.innerHTML = `
+        ✅ <b>${data.name}</b><br>
+        Lớp: ${data.class}<br>
+        Ngày: ${data.date}<br>
+        Giờ: ${data.time}
+      `;
+
+        img.src = API_BASE + data.image;
+        img.style.display = "block";
+      } else {
+        resultBox.innerText = data.message;
+        img.style.display = "none";
+      }
+    })
+    .catch(() => {
+      resultBox.innerText = "❌ Không kết nối được server";
+    });
+
+  function normalizeDate(input) {
+    input = input.replace(/-/g, "/"); // cho phép 1-1-2025
+    const parts = input.split("/");
+
+    if (parts.length !== 3) return null;
+
+    let [day, month, year] = parts;
+
+    if (year.length === 2) year = "20" + year;
+
+    day = day.padStart(2, "0");
+    month = month.padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
+}
